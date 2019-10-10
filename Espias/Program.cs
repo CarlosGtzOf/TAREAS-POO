0203f209654cc3df8c2c03990307182bf739d356
CarlosGtzOf <carlos.paniagua19@tectijuana.edu.mx>
2019-10-09 23:15:07 -0700

    domin/esp/dur

---
+++ Espias/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Espias
+{
+    class Persona
+    {
+        public string nombre;
+        public string apellido;
+
+        public Persona()
+        {
+            nombre = "Fulano";
+            apellido = " De Tal";
+        }
+    }
+
+    class Anonymus
+    {
+       public static void anonimiza(Persona p)
+        {
+            p.nombre = "xxxxxxxxxxxxxx";
+            p.apellido = " tu madre";
+        }
+        public static void cambia(ref Persona p)
+        {
+            p = new Persona();
+            p.nombre = "new";
+        }
+    }
+    
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona p = new Persona();
+            Console.WriteLine(p.nombre);
+            Anonymus.anonimiza(p);
+            Console.WriteLine(p.nombre);
+            Anonymus.cambia(ref p);
+            Console.WriteLine(p.nombre);
+
+        }
+    }
+}
---
+++ duracion/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace duracion
+{
+
+    class Duracion
+    {
+        private int segundos;
+
+        /*public Duracion(int h, int m, int s)
+        {
+            horas = h;
+            minutos = m;
+            segundos = s;
+        }*/
+        public Duracion(int h, int m, int s)
+        {  
+            segundos = h * 60 * 60;
+            segundos += m * 60;
+            segundos += s;
+        }
+        public void print()
+        {
+            Console.WriteLine($"{segundos}");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Duracion P = new Duracion(02, 15, 12);
+            Duracion C = new Duracion(00, 02, 15);
+            Duracion Pa = new Duracion(02, 00, 10);
+            P.print();
+            C.print();
+            Pa.print();
+
+        }
+    }
+}
