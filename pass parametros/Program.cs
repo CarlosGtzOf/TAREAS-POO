950b3b722e0906e0732f35ef9df6805861e69ffe
CarlosGtzOf <carlos.paniagua19@tectijuana.edu.mx>
2019-10-18 12:40:31 -0700

    paso de parametros
    ref

---
+++ pass parametros/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace pass_parametros
+{
+    class Program
+    {
+        class Peliculas
+        {
+            public string fav;
+            public Peliculas(string fav)
+            {
+                this.fav = fav;
+            }
+            
+            public void print()
+            {
+                Console.WriteLine($"mi pelicula favorita es {fav}");
+            }
+        }
+
+        class CamPel
+        {
+            public static void wow(ref Peliculas f)
+            {
+                f.fav = "Capitan America";
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Peliculas f = new Peliculas("harry potter");
+            CamPel.wow(ref f);
+
+            f.print();
+            
+        }
+    }
+}
