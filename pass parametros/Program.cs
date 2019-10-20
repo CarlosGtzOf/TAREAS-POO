1ac8a89a1b24f165e1658c452ae7ad3c49fcfcfa
Carlos Adrian Paniagua Gutierrez <carlos.paniagua19@tectijuana.edu.mx>
2019-10-20 14:40:42 -0700

    actualizacion
    pase de parametros completo

--- pass parametros/Program.cs
+++ pass parametros/Program.cs
@@ -7,6 +7,7 @@ namespace pass_parametros
         class Peliculas
         {
             public string fav;
+            private int e;
             public Peliculas(string fav)
             {
                 this.fav = fav;
@@ -14,7 +15,12 @@ namespace pass_parametros
             
             public void print()
             {
-                Console.WriteLine($"mi pelicula favorita es {fav}");
+                Console.WriteLine($"mi pelicula favorita es {fav}\nEl numero es: {e}");
+            }
+
+            public void numero(in int w)
+            {
+                this.e = w + 2;
             }
         }
 
@@ -26,13 +32,30 @@ namespace pass_parametros
             }
         }
 
+        class suma
+        {
+            public static void suma1(int a, int b, out int r)
+            {
+                r = a + b;
+            }
+
+            public static int suma1(int a, int b)
+            {
+                return a + b;
+            }
+        }
+
         static void Main(string[] args)
         {
+            int x = 2, num1 = 2, num2 = 3, res = 0;
             Peliculas f = new Peliculas("harry potter");
+
+            suma.suma1(num1, num2, out res);
             CamPel.wow(ref f);
+            f.numero(in x);
 
             f.print();
-            
+            Console.WriteLine($"el resultado de la suma es {res}");           
         }
     }
 }
