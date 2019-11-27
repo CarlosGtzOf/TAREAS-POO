0088a7b04da21af65c511dc1ce0cc1a4baeb5465
CarlosGtzOf <carlos.paniagua19@tectijuana.edu.mx>
2019-11-26 19:21:02 -0800

    Update
    Se agregaron ultimas tareas

---
+++ Stack/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace stack
+{
+    class Stack<T>
+    {
+        private T[] elementos;
+        public int tamaño;
+        private int apuntador = 0;
+
+        public Stack():this(100)
+        {
+
+        }
+
+        public Stack(int Tamaño)
+        {
+            tamaño = Tamaño;
+            elementos = new T[Tamaño];
+        }
+        
+        public void Push(T elemento)
+        {
+            elementos[apuntador] = elemento;
+            apuntador++;
+        }
+
+        public T Pop()
+        {
+            apuntador--;
+            return elementos[apuntador];
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<string> nombres = new Stack<string>(); 
+            nombres.Push("Hola");
+            nombres.Push(" ");
+            nombres.Push("Mundo");
+            for(int i = 0; i < 3; i++)
+            {
+                Console.Write(nombres.Pop());
+            }
+        
+        }
+    }
+}
