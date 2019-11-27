22e181a566a4abaac36714a88e8c58e0a017a323
CarlosGtzOf <carlos.paniagua19@tectijuana.edu.mx>
2019-11-26 19:05:27 -0800

    correcciones de pila

--- pila/Program.cs
+++ pila/Program.cs
@@ -2,53 +2,74 @@
 
 namespace pila
 {
-    public class Pila<T>
-        {
-            readonly int tamaño;
-            int apuntador = 0;
-            T[] element;
-
-            public Pila(int size)
-            {
-                tamaño = size;
-                element = new T[tamaño];
-            }
-
-            public void Push(T elemento)
-            {
-                if(apuntador >= tamaño)
-                {
-                    throw new StackOverflowException();
-                    element[apuntador] = elemento;
-                    apuntador++;    
-                }
-                else
-                {
-                    //error
-                }
-            }
-
-            public T Pop()
-            {
-                apuntador--;
-                if(apuntador >= 0)
-                {
-                    return element[apuntador];
-                }
-                else
-                {
-                    apuntador = 0;
-                    throw new InvalidOperationException("no se puede apuntar en una pila vacia");
-                }
-            }
+    public class Pila< T >
+    {
+        readonly int tamaño;
+        int apuntador = 0;
+        T[] elementos;
+            
+        public Pila() : this(100)
+	    {
 
         }
+
+        public Pila(int size)
+        {
+            tamaño = size;
+            elementos = new T[tamaño];
+        }
+
+        public void Push(T elemento)
+        {
+            if(apuntador>=tamaño)
+            {
+                throw new StackOverflowException();
+            }
+            else
+            {
+                elementos[apuntador]=elemento;
+                apuntador++;
+            }
+        }
+
+        public T Pop()
+        {        
+    
+            if(apuntador>0)
+            {      
+                T elemento=elementos[0];
+                for (int i = 1; i < apuntador; i++)
+                {
+                elementos[i-1]=elementos[i];
+                }   
+                apuntador--;
+                return elemento;
+            }
+            else
+            {
+                apuntador=0;
+                throw new InvalidOperationException("stack vacio");
+            }
+        }
+
+    }
 
     class Program
     {
 
         static void Main(string[] args)
         {
+            Pila<string> list = new Pila<string>();
+            list.Push("Mundo");
+            list.Push(" ");
+            list.Push("Hola");
+
+            for(int i = 0; i < 3; i++)
+            {
+                Console.WriteLine(list.Pop());
+            }
+            
+            
 
         }
     }
